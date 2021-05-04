1c272444508a2707114b765d2ad71db12e1891d9
Xpost2000 <xpost2000cod@gmail.com>
2021-05-04 15:12:53 -0400

    DOMIntoMarkdown parser
    This will be used for the RichTextEditor, which is a more convenient way of writing articles than the previous method of writing markdown raw.
    
    It does not handle images. It only handles text decoration so far.

---
+++ src/utils/DOMIntoMarkdown.js
@@ -0,0 +1,38 @@
+/*
+    Inhouse DOM to Markdown interpreter.
+*/
+const surrounder = (sym) => (content) => `${sym}${content}${sym}`;
+const surrounders = {
+  "B": surrounder("**"),
+  "I": surrounder("_"),
+  "U": surrounder("__"),
+};
+const safeLambdaCall = (fn, ...rest) => (...rest) => (fn) ? fn(...rest) : rest;
+
+function renderElement(root, textContents) {
+  if (textContents !== "") {
+      if (root.tagName === "P") {
+          return textContents + "\n";
+      } else if (root.tagName[0] === 'H') {
+          return "#".repeat(Number(root.tagName[1])) + " " + textContents;
+      } else if (root.tagName === "LI") { // List item
+          return (root.parentElement.nodeName === "OL") ?
+              `${Array.from(root.parentElement.childNodes).indexOf(root) + 1}. ${textContents}\n` : `- ${textContents}\n`;
+      }
+
+      return safeLambdaCall(surrounders[root.tagName])(textContents)[0];
+  } else {
+      return (root.tagName === "P") ? "\\\n" : "";
+  }
+
+  return textContents + "\n";
+}
+
+
+export function renderDomAsMarkdown(root) {
+  return (root.hasChildNodes()) ?
+      renderElement(root, Array.from(root.childNodes)
+                     .reduce((output, node) => output +
+                             renderDomAsMarkdown(node), ""))
+      : (root.nodeType === 3) ? root.textContent : "";
+}
