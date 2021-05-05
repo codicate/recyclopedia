6e2bf0df412563cf0b8d665e9828698bfe43a251
Xpost2000 <xpost2000cod@gmail.com>
2021-05-04 22:15:57 -0400

    Automatically synchronized rich text editor state
    I am completely aware that document.execCommandState may be deprecated
    however there are no other commonly documented alternatives, and
    literally any other solution is probably closed source.

--- src/pages/Admin/Editors/RichTextEditor.jsx
+++ src/pages/Admin/Editors/RichTextEditor.jsx
@@ -18,10 +18,14 @@ document.execCommand("defaultParagraphSeparator", false, "p");
 export function RichTextEditor({ submissionHandler, currentArticle }) {
   const editableTitleDOMRef = useRef();
   const editableAreaDOMRef = useRef();
+
   const [widgetStates, updateWidgetState] = useState({
     "decoration": null,
     "heading": null,
-    "list": null
+    "list": null,
+    "bold": null,
+    "italic": null,
+    "underline": null,
   });
 
   function executeRichTextCommand(commandName, optionalArgument) {
@@ -31,10 +35,27 @@ export function RichTextEditor({ submissionHandler, currentArticle }) {
     }
   }
 
+  function queryRichTextCommand(command, wantedValue) {
+    if (wantedValue) {
+      return document.queryCommandValue(command);
+    } else {
+      return document.queryCommandState(command);
+    }
+  }
+
+  function synchronizeCommandStateToWidgetBar() {
+    updateWidgetState({
+      "bold": queryRichTextCommand("bold") ? "bold" : null,
+      "underline": queryRichTextCommand("underline") ? "underline" : null,
+      "italic": queryRichTextCommand("italic") ? "italic" : null,
+      "heading": queryRichTextCommand("heading", true),
+    });
+  }
+
   const toolBarRichWidgets = {
-    bold: { name: "Bold", display: <b>B</b>, command: "bold", category: "decoration" },
-    italic: { name: "Italic", display: <emph>I</emph>, command: "italic", category: "decoration" },
-    underline: { name: "Underline", display: <u>UL</u>, command: "underline", category: "decoration" },
+    bold: { name: "Bold", display: <b>B</b>, command: "bold" },
+    italic: { name: "Italic", display: <emph>I</emph>, command: "italic" },
+    underline: { name: "Underline", display: <u>UL</u>, command: "underline" },
     h1: { name: "Heading 1", display: <b>h1</b>, command: "heading", argument: "H1", category: "heading" },
     h2: { name: "Heading 2", display: <b>h2</b>, command: "heading", argument: "H2", category: "heading" },
     h3: { name: "Heading 3", display: <b>h3</b>, command: "heading", argument: "H3", category: "heading" },
@@ -65,13 +86,23 @@ export function RichTextEditor({ submissionHandler, currentArticle }) {
             ([widgetId, widget]) => (<button
               key={widgetId}
               id={widgetId}
-              style={(widgetStates[widget.category] === widgetId) ? activeWidgetCategory : {}}
+              style={(widgetStates[(widget.category) ? widget.category : widgetId] === widgetId) ? activeWidgetCategory : {}}
               onClick={
                 (_) => {
                   const currentSelection = window.getSelection();
                   console.log(currentSelection.type);
-                  console.log(widgetStates);
-                  updateWidgetState(dictionaryUpdateKey(widgetStates, widget.category, () => widgetId ));
+                  console.log(document.queryCommandState("bold"));
+
+                  // TODO
+                  updateWidgetState(dictionaryUpdateKey(widgetStates,
+                    (widget.category) ? widget.category : widgetId, (original) => {
+                      console.log(original, original === widgetId);
+                      if (original === widgetId) {
+                        return null;
+                      } else {
+                        return widgetId;
+                      }
+                    }));
                   if (widget.argument) {
                     executeRichTextCommand(widget.command, widget.argument);
                   } else {
@@ -89,6 +120,7 @@ export function RichTextEditor({ submissionHandler, currentArticle }) {
         <div
           contentEditable={true}
           className={styles.article}
+          onSelect={synchronizeCommandStateToWidgetBar}
           dangerouslySetInnerHTML={
             { __html: renderMarkdown(preprocessMarkdown((currentArticle) ? currentArticle.content : "Begin typing your article.")) }
           }
