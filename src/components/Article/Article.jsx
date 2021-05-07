1a9b58728ad590df7b1a3e5c35fdf09460cc213c
Xpost2000 <xpost2000cod@gmail.com>
2021-05-07 19:20:08 -0400

    Changed access to allow for full article information
    While this is technically not as pure (there is at least one additional
    referential copy that may not be needed).
    
    This is a quickfix to pass the entirety of the article to allow for
    changes

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -11,69 +11,71 @@ import Admin from 'pages/Admin/Admin';
 import { ApplicationContext } from 'App';
 
 const md = require('markdown-it')(
-  {
-    html: true,
-    breaks: true,
-    linkify: true,
-    typographer: true,
-  }
+    {
+        html: true,
+        breaks: true,
+        linkify: true,
+        typographer: true,
+    }
 );
 
-export function buildFromJSON({ name, content, api, articlesData, setArticlesData }) {
-  return (
-    <Route key={name} exact path={validPageLink(name)}>
-      <Article name={name} content={content} api={api} articlesData={articlesData} setArticlesData={setArticlesData} />
-    </Route>
-  );
+export function buildFromJSON({ article, api, articlesData, setArticlesData }) {
+    const {name} = article;
+    return (
+        <Route key={name} exact path={validPageLink(name)}>
+          <Article article={article} api={api} articlesData={articlesData} setArticlesData={setArticlesData} />
+        </Route>
+    );
 }
 
 export function ArticleRender({ name, content }) {
-  return (
-    <div>
-      <h1 className={styles.title}> {name} </h1>
-      <MarkdownRender className={styles.article}>
-        {preprocessMarkdown(content)}
-      </MarkdownRender>
-    </div>
-  );
+    return (
+        <div>
+          <h1 className={styles.title}> {name} </h1>
+          <MarkdownRender className={styles.article}>
+            {preprocessMarkdown(content)}
+          </MarkdownRender>
+        </div>
+    );
 }
 
-export function Article({ name, content, api, articlesData, setArticlesData }) {
-  const [adminEditView, updateAdminEditView] = React.useState(false);
-  const history = useHistory();
+export function Article({ article, api, articlesData, setArticlesData }) {
+    const [adminEditView, updateAdminEditView] = React.useState(false);
+    const {name, content} = article;
+    const history = useHistory();
 
-  const context = useContext(ApplicationContext);
+    const context = useContext(ApplicationContext);
 
-  return <>
-    {
+    return <>
+  {
       (context.isAdmin) && (
-        <>
-          <button
-            onClick={() => {
-              api.deleteArticle(name);
-              history.push('/');
-              articlesData.articles = articlesData.articles.filter(item => item.name !== name);
-              setArticlesData(articlesData);
-            }}
-          >
-            Delete Page
+          <>
+            <button
+              onClick={() => {
+                  api.deleteArticle(name);
+                  history.push('/');
+                  articlesData.articles = articlesData.articles.filter(item => item.name !== name);
+                  setArticlesData(articlesData);
+              }}
+            >
+              Delete Page
             </button>
-          <button
-            onClick={() => updateAdminEditView(!adminEditView)}
-          >
-            Edit This Page
+            <button
+              onClick={() => updateAdminEditView(!adminEditView)}
+            >
+              Edit This Page
             </button>
-        </>
+          </>
       )
-    }
-    { (adminEditView)
-      ? (<Admin
-        currentArticle={{ name, content }}
-        api={api}
-        articlesData={articlesData}
-        setArticlesData={setArticlesData} />
+  }
+  { (adminEditView)
+    ? (<Admin
+                    currentArticle={{... article}}
+                    api={api}
+                    articlesData={articlesData}
+                    setArticlesData={setArticlesData} />
       ) : 
-      <ArticleRender name={name} content={content} />
-    }
-  </>;
+    <ArticleRender name={name} content={content} />
+  }
+</>;
 }
