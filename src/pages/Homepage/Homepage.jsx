2754eb8b6274d537e5966fb418255dd3799d5909
Jerry Zhu <xpost2000cod@gmail.com>
2021-05-22 21:21:33 -0400

    Spiffier homepage skeleton
    Has a featured article and random article section. For obvious reasons
    they should not be in a list format, but rather they should be a grid
    instead.

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,18 +1,67 @@
 import { Link } from 'react-router-dom';
 
 import styles from 'pages/Homepage/Homepage.module.scss';
-
-
+import { randomElt } from 'utils/functions';
+import { MarkdownRender } from 'components/Article/RenderMarkdown';
 
 import { useState, useContext } from 'react';
 import { ApplicationContext } from 'App';
 
-function Homepage({ api, setArticlesData }) {
+function ArticleShowcase({articlesData: {articles}}) {
+  const {name, content} = randomElt(articles);
+  // console.log( randomElt(articlesData));
+  return (
+    <>
+      <h2>Featured Article</h2>
+      <div
+      style={
+        {
+          borderStyle: "solid",
+          borderColor: "black",
+          borderWidth: "1px",
+          padding: "0.5em",
+          maxHeight: "30%",
+        }
+      }>
+        <p>TODO what is the best way to determine a featured article?</p>
+        <p>Honestly, that might have to be manually audited since that depends on a lot of things.</p>
+      </div>
+      <h2>Random Article</h2>
+      <div 
+      style={
+        {
+          borderStyle: "solid",
+          borderColor: "black",
+          borderWidth: "1px",
+          padding: "0.5em",
+          maxHeight: "30%",
+        }
+      }>
+        <u><h2>{name}</h2></u>
+        <MarkdownRender className={styles.searchResult}>
+          {`${content.substr(0, 800).replaceAll(/(@@.*)|(@@.*@@)/g, '')}`}
+        </MarkdownRender>
+      </div>
+    </>
+  );
+}
+
+function Homepage({ api, articlesData }) {
   const context = useContext(ApplicationContext);
 
   return (
     <>
       <h1>Welcome to Recyclopedia</h1>
+      <p>
+        Recyclopedia is a freely accessible wiki designed to be a complete source for 
+        environmentally friendly actions. While it is primarily meant to be a comprehensive
+        source of ways to recycle items appropriately. It may also contain other methods of
+        sustaining an environmentally friendly lifestyle.
+      </p>
+      <br></br>
+      <p>This is developed by the <a href="https://www.projectenv.org/"><u>Environment Project</u></a></p>
+      <br></br>
+      <ArticleShowcase articlesData={articlesData}></ArticleShowcase>
     </>
   );
 }
