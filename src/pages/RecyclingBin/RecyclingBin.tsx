53ff19f76f00e231dfa381407e736de5760b5cc9
codicate <codicate@gmail.com>
2021-07-05 14:44:21 -0400

    recylebin refactor

--- src/pages/RecyclingBin/RecyclingBin.tsx
+++ src/pages/RecyclingBin/RecyclingBin.tsx
@@ -1,47 +1,45 @@
 import styles from "pages/RecyclingBin/RecyclingBinPage.module.scss";
-import { Link } from "react-router-dom";
-
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
-
-import Button from "components/UI/Button";
+import { Link, useHistory } from "react-router-dom";
 
 import { useAppSelector, useAppDispatch } from "app/hooks";
 import { selectArticlesData, deleteArticle, restoreArticle } from "app/articlesSlice";
 import { LoginType, selectLoginType } from "app/adminSlice";
 
 import { validPageLink, dictionaryUpdateKey } from "utils/functions";
-import CheckboxButton from "components/UI/CheckboxButton";
+import Button from "components/UI/Button";
+
 
 // little filler component as to not complict the RecyclingBin
 type DaysLeft = {
   value: number,
 };
-function DaysLeft({value} : DaysLeft) {
-  const inlineStyle = {
-    backgroundColor: function() {
+
+function DaysLeft({ value }: DaysLeft) {
+  const statusBg = {
+    backgroundColor: function () {
       if (value <= 5) {
         return "red";
       } else if (value <= 15) {
         return "yellow";
       }
       return "transparent";
-    }(),
-    marginLeft: "1em",
-    fontSize: "1.2em",
-    display: "inline-block",
-    minWidth: "3em",
+    }()
   };
 
   return (
     <>
-      <span style={inlineStyle}>{value} Days Left</span>
-    </>);
+      <span
+        className={styles.daysLeft}
+        style={statusBg}
+      >
+        {value} Days Left
+      </span>
+    </>
+  );
 }
 
-/*
-    No admin check required. Things will already die here.
-*/
+
+// No admin check required. Things will already die here.
 function RecyclingBin() {
   const articlesData = useAppSelector(selectArticlesData);
   const currentLoginType = useAppSelector(selectLoginType);
@@ -49,32 +47,28 @@ function RecyclingBin() {
   const history = useHistory();
 
   return (
-    <div className={styles.index}>
+    <div id={styles.recyclingBin}>
       <h2>Welcome to the Recycling Bin</h2>
-      <p>This is where articles go to die! If you&apos;ve accidently deleted
-      an article. You can recover them here! Or you can dispose of them permenantly here!</p>
+      <p>
+        This is where articles go to die! If you&apos;ve accidently deleted an article. You can recover them here! Or you can dispose of them permenantly here!
+      </p>
 
       {
-        (articlesData.recycledArticles.length === 0) ?
+        (articlesData.recycledArticles.length === 0) ? (
           <>
-            <p style={{
-              marginTop: "3em",
-              textAlign: "center",
-              fontSize: "2em",
-            }}>
-              <b>The Recycling Bin is Empty!</b>
+            <p id={styles.emptyMsg}>
+              The Recycling Bin is Empty!
             </p>
           </>
-          : articlesData.recycledArticles
-            .map(({ name, content, pendingDaysUntilDeletion }) => (
-              <>
-                {
-                  /*
-                          I may install material-icons since I kind of want to replace these with
-                          icons... Although technically I should ask for permission first.
-                        */
-                }
-                <p key={name} >
+        ) : (
+          articlesData.recycledArticles.map(({ name, content, pendingDaysUntilDeletion }) => (
+            <>
+              <div key={name} >
+                <Link to={"/admin/recycling_bin" + validPageLink(name)}>
+                  {name}
+                  <DaysLeft value={pendingDaysUntilDeletion} />
+                </Link>
+                <div>
                   <Button
                     styledAs="oval"
                     onClick={async () => {
@@ -84,7 +78,7 @@ function RecyclingBin() {
                       }
                     }}
                   >
-                        Restore
+                    Restore
                   </Button>
                   <Button
                     styledAs="oval"
@@ -95,15 +89,13 @@ function RecyclingBin() {
                       }
                     }}
                   >
-                        Delete
+                    Delete
                   </Button>
-                  <Link to={"/admin/recycling_bin" + validPageLink(name)}>
-                    {name}
-                    <DaysLeft value={pendingDaysUntilDeletion}/>
-                  </Link>
-                </p>
-              </>
-            ))
+                </div>
+              </div>
+            </>
+          ))
+        )
       }
     </div>);
 }
