76b9c841ca19320c334b7b071a7facca71f7bea3
Jerry Zhu <xpost2000cod@gmail.com>
2021-09-26 22:27:52 -0400

    Enabled cron in strapi to allow for recycled article porting!

--- strapi-instance/config/functions/cron.js
+++ strapi-instance/config/functions/cron.js
@@ -1,21 +1,13 @@
 'use strict';
 
-/**
- * Cron config that gives you an opportunity
- * to run scheduled jobs.
- *
- * The cron format consists of:
- * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
- *
- * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
- */
-
 module.exports = {
-  /**
-   * Simple example.
-   * Every monday at 1am.
-   */
-  // '0 1 * * 1': () => {
-  //
-  // }
+  /*
+    Ticks at the start of every new day
+    to clean up recycled articles.
+
+    This obviously doesn't happen quite yet!
+  */
+   "0 0 0 * * *": function () {
+    console.log("Recycle Article collection!");
+  }
 };
--- strapi-instance/config/server.js
+++ strapi-instance/config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: {
+    enabled: true,
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '919775eca17a22ba375d21efbde0f819'),
